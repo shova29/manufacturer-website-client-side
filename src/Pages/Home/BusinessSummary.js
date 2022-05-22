ead212caa27cf88be4f666f95f1a03db22456276
shova29 <halima2996@gmail.com>
2022-05-23 02:30:42 +0600

    business summary added

---
+++ src/Pages/Home/BusinessSummary.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+const BusinessSummary = () => {
+  return (
+    <div className="max-w-7xl mx-auto mt-12 px-12 justify-center items-center text-center">
+      <div className="lg:w-60 lg:mr-8 stats stats-vertical lg:stats-horizontal shadow bg-primary text-white ">
+        <div className="stat place-items-center">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Downloads</div>
+          <div className="stat-value">31K</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>{" "}
+      </div>
+      <div className="lg:w-60 lg:mr-8 stats stats-vertical lg:stats-horizontal shadow bg-primary text-white ">
+        <div className="stat place-items-center">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Downloads</div>
+          <div className="stat-value">31K</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>{" "}
+      </div>
+      <div className="lg:w-60 stats stats-vertical lg:stats-horizontal shadow bg-primary text-white">
+        <div className="stat place-items-center">
+          <div class="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Downloads</div>
+          <div className="stat-value">31K</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>{" "}
+      </div>
+    </div>
+  );
+};
+
+{
+  /*  <div className="stat place-items-center">
+        <div class="stat-figure text-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-8 h-8 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        </div>
+        <div className="stat-title">New Users</div>
+        <div className="stat-value">4,200</div>
+        <div className="stat-desc">400 (22%)</div>
+      </div>{" "}
+      <div className="stat place-items-center">
+        <div class="stat-figure text-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-8 h-8 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+        </div>
+        <div className="stat-title">New Registers</div>
+        <div className="stat-value">1,200</div>
+        <div className="stat-desc">90 (14%)</div>
+      </div>
+    </div> */
+}
+
+export default BusinessSummary;
